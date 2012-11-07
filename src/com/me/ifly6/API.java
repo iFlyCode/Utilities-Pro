012c92a760381f38c7c854c207e4ef99bff19074
Kyle Swartley <kyleswartley@gmail.com>
2012-11-07 18:34:52 -0500

    Let's DO THIS!

--- src/com/me/ifly6/API.java
+++ src/com/me/ifly6/API.java
@@ -2,6 +2,20 @@ package com.me.ifly6;
 
 public interface API {
 
+	/**
+	 * @return whether the plugin should let the command through to the OS after execution.
+	 */
 	public boolean blocksCommand();
 	
+	/**
+	 * @param command, the command with args passed to the plugin by the console.
+	 * @return 0 if all went well
+	 */
+	public int execute(String[] command);
+	
+	/**
+	 * 
+	 * @return String array of commands the console should hand off to the plugin
+	 */
+	public int claimedCommands();
 }
--- src/com/me/ifly6/plugin/PluginSample.java
+++ src/com/me/ifly6/plugin/PluginSample.java
@@ -1,5 +1,7 @@
 package com.me.ifly6.plugin;
 
-public class PluginSample {
+import com.me.ifly6.API;
+
+public class PluginSample implements API{
 
 }
